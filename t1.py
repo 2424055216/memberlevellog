702e4f323d3e209193f2f4e6de0eb7794ca983a8
2424055216 <2424055216@qq.com>
2022-06-08 13:49:15 +0800

    更改t1

--- t1.py
+++ t1.py
@@ -7,7 +7,6 @@ sql = """insert into em_h5_record (id,name,url,member_id,key1,key2,open_id,page_
 del_flag,creator,creator_id,created_at,updator,updator_id,updated_at,privacy_policy) 
 values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
 
-
 for i in range(1000, 10000):
     value = (i, '0325', 'http', '', '', '', 'oghzNwMfzmJJPICngsWvF8aooTuI', 1840, '?app', 'nj', 0, '', '', '2021-03-25 17:15:18', '', '', '2021-05-19 15:03:52', '')
     cursor.execute(sql, value)
